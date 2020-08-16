2f4e1240cffc06ea2f566ee6d1e83d1b424f4e7e
kotkaaa <abamka@hotmail.com>
2020-08-16 12:23:01 +0300

    add category CRUD + migrations

--- backend/views/category/view.php
+++ backend/views/category/view.php
@@ -34,8 +34,28 @@ $this->params['breadcrumbs'][] = $this->title;
             'title',
             'description:raw',
             'alias',
-            'icon',
-            'image',
+            [
+                'attribute' => 'icon',
+                'format' => 'raw',
+                'value' => function () use ($model) {
+                    if ($model->icon) {
+                        return Html::img(\Yii::$app->params['frontUrl'] . '/uploads/category/' . $model->icon, ['class' => 'img-thumbnail']);
+                    }
+
+                    return null;
+                }
+            ],
+            [
+                'attribute' => 'image',
+                'format' => 'raw',
+                'value' => function () use ($model) {
+                    if ($model->image) {
+                        return Html::img(\Yii::$app->params['frontUrl'] . '/uploads/category/' . $model->image, ['class' => 'img-thumbnail']);
+                    }
+
+                    return null;
+                }
+            ],
             'meta_title',
             'meta_description:ntext',
             'meta_keywords:ntext',
